e5d252d442508aa198e527cb816e0bb55d2ac793
franklintackettjr <franklin.tackett@okstate.edu>
2017-05-28 17:50:30 -0500

    Basic sign ui completed

---
+++ Build/Intermediates/Pods.build/Debug-iphonesimulator/GTMSessionFetcher.build/Objects-normal/i386/GTMSessionFetcherLogging.d
@@ -0,0 +1,6 @@
+dependencies: \
+  /Users/franklin/Desktop/Playground/AppDev/devslopes-social/Pods/GTMSessionFetcher/Source/GTMSessionFetcherLogging.m \
+  /Users/franklin/Desktop/Playground/AppDev/devslopes-social/Build/Intermediates/Pods.build/Debug-iphonesimulator/GTMSessionFetcher.build/module.modulemap \
+  /Users/franklin/Desktop/Playground/AppDev/devslopes-social/Pods/Target\ Support\ Files/GTMSessionFetcher/GTMSessionFetcher-prefix.pch \
+  /Users/franklin/Desktop/Playground/AppDev/devslopes-social/Pods/GTMSessionFetcher/Source/GTMSessionFetcherLogging.h \
+  /Users/franklin/Desktop/Playground/AppDev/devslopes-social/Pods/GTMSessionFetcher/Source/GTMSessionFetcher.h
---
+++ Build/Intermediates/Pods.build/Debug-iphonesimulator/GoogleToolboxForMac.build/Objects-normal/i386/GoogleToolboxForMac_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/franklin/Desktop/Playground/AppDev/devslopes-social/Build/Intermediates/Pods.build/Debug-iphonesimulator/GoogleToolboxForMac.build/DerivedSources/GoogleToolboxForMac_vers.c
