46ef9072ec11fe4ef42fe3e67514f13bf1ef9b3e
franklintackettjr <franklin.tackett@okstate.edu>
2017-05-29 10:33:24 -0500

    loaded some data to load in app feed

--- Build/Intermediates/devslopes-social.build/Debug-iphonesimulator/devslopes-social.build/Objects-normal/x86_64/devslopes_social-Swift.h
+++ Build/Intermediates/devslopes-social.build/Debug-iphonesimulator/devslopes-social.build/Objects-normal/x86_64/devslopes_social-Swift.h
@@ -221,7 +221,6 @@ SWIFT_CLASS("_TtC16devslopes_social8PostCell")
 @property (nonatomic, weak) IBOutlet UITextView * _Null_unspecified caption;
 @property (nonatomic, weak) IBOutlet UILabel * _Null_unspecified likesLbl;
 - (void)awakeFromNib;
-- (void)setSelected:(BOOL)selected animated:(BOOL)animated;
 - (nonnull instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString * _Nullable)reuseIdentifier OBJC_DESIGNATED_INITIALIZER SWIFT_AVAILABILITY(ios,introduced=3.0);
 - (nullable instancetype)initWithCoder:(NSCoder * _Nonnull)aDecoder OBJC_DESIGNATED_INITIALIZER;
 @end
--- devslopes-social/PostCell.swift
+++ devslopes-social/PostCell.swift
@@ -15,17 +15,20 @@ class PostCell: UITableViewCell {
     @IBOutlet weak var postImg: UIImageView!
     @IBOutlet weak var caption: UITextView!
     @IBOutlet weak var likesLbl: UILabel!
+    
+    var post: Post!
 
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
     
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    func configureCell(post: Post) {
+        self.post = post
+        self.caption.text = post.caption
+        //chech for errors here
+        self.likesLbl.text = String(post.likes)
+        
     }
 
 }
